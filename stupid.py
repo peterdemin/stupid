b19a6d08552827a50ab3450b67b6ea89dd452e15
Peter <petr.demin@nih.gov>
2015-12-15 16:09:02 -0500

    Added most important reminders

--- stupid.py
+++ stupid.py
@@ -1,7 +1,62 @@
+import datetime
+import functools
 import os
-import slack
+import sys
+import time
+
+import schedule
 import slack.chat
 
 
-slack.api_token = os.environ['STUPID_TOKEN']
-slack.chat.post_message('#loud-launches', 'Eat some!', username='Stupid')
+def weekday(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if datetime.datetime.now().weekday() in range(0, 6):
+            return func(*args, **kwargs)
+    return wrapper
+
+
+def post(message):
+    return slack.chat.post_message('#loud-launches', message, username='Stupid')
+
+
+@weekday
+def eat_some():
+    return post('Eat some!')
+
+
+@weekday
+def print_some():
+    print('ok')
+
+
+def print_jobs():
+    for job in schedule.default_scheduler.jobs:
+        print(job.next_run)
+
+
+def main():
+    slack.api_token = os.environ['STUPID_TOKEN']
+    schedule.every().day.at("11:55").do(eat_some)
+    schedule.every().day.at("15:55").do(eat_some)
+    schedule.every().day.at("17:15").do(post, 'Go home')
+    print_jobs()
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+def check():
+    schedule.every().day.at("11:55").do(print_some)
+    schedule.every().day.at("15:55").do(print_some)
+    schedule.every().day.at("17:15").do(print_some)
+    print_jobs()
+    schedule.default_scheduler.run_all()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'check':
+            check()
+            sys.exit(0)
+    main()
