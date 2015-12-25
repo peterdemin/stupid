1c4b670dc894b94488d2e9bf2c5de79d32e133c3
Petr Demin <petr.demin@nih.gov>
2015-12-25 00:52:47 -0500

    Moving Reader to ChatBot

--- stupid/__init__.py
+++ stupid/__init__.py
@@ -72,39 +72,6 @@ def read_new_messages(oldest_ts=None):
     return slack.channels.history(CHANNEL_ID, oldest=oldest_ts)['messages']
 
 
-class Reader(object):
-    def __init__(self, handler):
-        self.handler = handler
-        self.oldest_ts = None
-
-    def read(self):
-        messages = read_new_messages(self.oldest_ts)
-        if messages:
-            for message in messages:
-                if self.is_from_me(message):
-                    # Bot already replied, skip remaining messages
-                    logger.debug('Found own reply. Skipping older messages')
-                    break
-                text = message['text']
-                logger.debug('Parsing %s', text)
-                if self.has_trigger(text):
-                    logger.debug('Triggering %r', self.handler)
-                    response = self.handler.on_message(text)
-                    if response is not None:
-                        post_response = post(response)
-                        if hasattr(self.handler, 'on_posted'):
-                            self.handler.on_posted(post_response['message'])
-            self.oldest_ts = messages[0]['ts']
-
-    def has_trigger(self, message):
-        msg = message.lower()
-        return '<@{0}>'.format(MY_ID) in message and any(trigger in msg for trigger in self.handler.triggers)
-
-    @staticmethod
-    def is_from_me(message):
-        return message.get('username', None) == MY_USERNAME
-
-
 @weekday
 def go_home():
     return post("Russian, go home", color='warning')
