796d01b0e4537dec352e9aa1896231aa91fbed91
Petr <petr.demin@nih.gov>
2016-01-04 11:27:18 -0500

    Fixed MANIFEST.in for source distribution

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = "1.3.4"
+version = "1.3.5"
 
 if sys.argv[-1] == 'publish':
     try:
@@ -61,7 +61,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     entry_points={
         'console_scripts': [
