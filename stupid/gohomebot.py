61fdf94cdccfa7cf2a16d898c68521dc5d70eed7
Igor Sobolev <igr.sobolev@gmail.com>
2016-01-22 18:06:56 -0500

    Fixed "Go home" on weekend

--- stupid/gohomebot.py
+++ stupid/gohomebot.py
@@ -1,4 +1,5 @@
 from stupid.chatbot import ChatBot
+from stupid.utils import weekday
 
 
 class GoHomeBot(ChatBot):
@@ -6,5 +7,6 @@ class GoHomeBot(ChatBot):
         super(GoHomeBot, self).__init__(*args, **kwargs)
         self.schedule.every().day.at("17:15").do(self.post_go_home)
 
+    @weekday
     def post_go_home(self):
         return self.broker.post("Russian, go home", color='warning')
